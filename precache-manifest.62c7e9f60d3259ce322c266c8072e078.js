c717235273dc1323bd6355b5d76cd1d8f812c476
SaiKiran <skktangirala@gmail.com>
2020-05-10 18:53:32 +0530

    Updates

---
+++ precache-manifest.62c7e9f60d3259ce322c266c8072e078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e83f2507968e38a8894a0d90eca6994",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6306e9629c009d8665c5",
+    "url": "/my-portfolio/static/css/main.ec89d118.chunk.css"
+  },
+  {
+    "revision": "e83091fd4050547ba30f",
+    "url": "/my-portfolio/static/js/2.8d38f62f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.8d38f62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6306e9629c009d8665c5",
+    "url": "/my-portfolio/static/js/main.77321d2e.chunk.js"
+  },
+  {
+    "revision": "71f8fe099066da5003c6",
+    "url": "/my-portfolio/static/js/runtime-main.f6d94e01.js"
+  }
+]);
